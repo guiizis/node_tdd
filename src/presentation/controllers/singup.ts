20b126222a666112c675f33015b14ead2d8b51d1
guiizis <guigamoda@hotmail.com>
2022-06-20 21:55:15 -0300

    feat: adiciona confirmação de senha ao singup controller

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -3,7 +3,7 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 
 export class SignupController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields: string[] = ['name', 'email', 'password']
+    const requiredFields: string[] = ['name', 'email', 'password', 'passwordConfirmation']
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
