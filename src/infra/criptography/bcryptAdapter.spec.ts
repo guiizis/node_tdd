51ce0b60e76a0aa79a63073edfc7f593e0614075
guiizis <guigamoda@hotmail.com>
2022-06-30 18:20:03 -0300

    refactor: cria factory para sut do bcrypterAdapter

--- src/infra/criptography/bcryptAdapter.spec.ts
+++ src/infra/criptography/bcryptAdapter.spec.ts
@@ -1,24 +1,29 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bCryptAdapter'
 
+const salt = 12
+
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
     return await new Promise(resolve => resolve('hashed_value'))
   }
 }))
 
+const makeSUT = (): BcryptAdapter => {
+  const sut = new BcryptAdapter(salt)
+  return sut
+}
+
 describe('Bcrypt Adapater', () => {
   it('should call bcrypt with correct data', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSUT()
     const hashSpy = jest.spyOn(bcrypt, 'hash')
     await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
   it('should return a hash on success', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const sut = makeSUT()
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hashed_value')
   })
