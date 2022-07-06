a34d20554e4bbe9cb79252368c9ddaac26d0a8ce
guiizis <guigamoda@hotmail.com>
2022-07-06 13:48:24 -0300

    feat: adiciona validacao de conexao para o mongoDB

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: '<rootDir>/src',
   preset: '@shelf/jest-mongodb',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts','!<rootDir>/src/main/**'],
   coverageDirectory: 'coverage',
   transform: {
     '.+\\.ts$': 'ts-jest'
---
+++ src/infra/db/mongodb/helpers/mongoHelper.spec.ts
@@ -0,0 +1,19 @@
+import { MongoHelper as sut } from './mongoHelper'
+
+describe('Mongo Helper Test', () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+
+  it('should reconnect if mongoDB is down', async () => {
+    let accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+    await sut.disconnect()
+    accountCollection = await sut.getCollection('accounts')
+    expect(accountCollection).toBeTruthy()
+  })
+})
