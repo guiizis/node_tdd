9b77af6d57a23f7f5718e73efae4ece6eed8e0e0
guiizis <guigamoda@hotmail.com>
2022-07-08 13:57:24 -0300

    refactor: adiciona refactor para decorator de log

--- src/presentation/controllers/singup/singup.spec.ts
+++ src/presentation/controllers/singup/singup.spec.ts
@@ -76,7 +76,7 @@ describe('SingUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError('Invalid Param'))
   })
 
   it('Should return 500 if emailValidator throws', async () => {
@@ -96,7 +96,7 @@ describe('SingUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError('Invalid Param'))
   })
 
   it('Should return 400 if a invalid email is provided', async () => {
