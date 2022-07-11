e86ab6c19f499f51d1b2fac2b6af20b332eb5bba
guiizis <guigamoda@hotmail.com>
2022-07-11 14:27:53 -0300

    refactor: adiciona logMongoRepository a singup

--- src/main/factories/singup.ts
+++ src/main/factories/singup.ts
@@ -1,6 +1,7 @@
 import { DBAddAccount } from '../../data/useCases/addAccount/dbAddAccount'
 import { BcryptAdapter } from '../../infra/criptography/bCryptAdapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/accountRepository/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/logRepository/log'
 import { SignupController } from '../../presentation/controllers/singup/singup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/emailValidatorAdapter'
@@ -13,5 +14,6 @@ export const makeSingUpController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DBAddAccount(bcryptAdapter, accountMongoRepository)
   const singUpController = new SignupController(emailValidatorAdapter, dbAddAccount)
-  return new LogControllerDecorator(singUpController)
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(singUpController, logMongoRepository)
 }
