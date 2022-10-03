e8de92d28bb4808a8fcb69742298e056604f4cbd
guiizis <guigamoda@hotmail.com>
2022-10-03 16:59:06 -0300

    refactor: remove codigo duplicado

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -7,20 +7,19 @@ import { EmailValidator } from '../singup/singupProtocols'
 export class LoginController implements Controller {
   constructor(private readonly emailValidator: EmailValidator, private readonly authenticator: Authentication) { }
 
-  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const { email, password } = httpRequest.body
-      if (!email) {
-        return await new Promise(resolve => resolve(badRequest(new MissingParamError('email'))))
+      const requiredFields: string[] = ['email', 'password']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-      if (!password) {
-        return await new Promise(resolve => resolve(badRequest(new MissingParamError('password'))))
-      }
-
       const isValid = this.emailValidator.isValid(email)
 
       if (!isValid) {
-        return await new Promise(resolve => resolve(badRequest(new InvalidParamError('email'))))
+        return badRequest(new InvalidParamError('email'))
       }
       await this.authenticator.auth(email, password)
     } catch (error) {
