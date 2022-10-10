4505c8002d40395dc0f71cfe0bb76f994f073cf6
guiizis <guigamoda@hotmail.com>
2022-10-10 09:22:01 -0300

    test: garante que accessToken sera nulo caso funcao load retorne nulo

--- src/domain/useCases/authentication/dbAuthentication.spec.ts
+++ src/domain/useCases/authentication/dbAuthentication.spec.ts
@@ -54,4 +54,11 @@ describe('dbAuthentication useCase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  it('Should return null if loadAccountByEmailRepository returns null', async () => {
+    const { loadAccountByEmailRepositoryStub, sut } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBe(null)
+  })
 })
