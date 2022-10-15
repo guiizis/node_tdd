b8b2c768197822335c0a36432fa4a1b89f8ae197
guiizis <guigamoda@hotmail.com>
2022-10-15 09:55:48 -0300

    test: garante throw do compare

--- src/infra/criptography/bcryptAdapter.spec.ts
+++ src/infra/criptography/bcryptAdapter.spec.ts
@@ -58,4 +58,12 @@ describe('Bcrypt Adapater', () => {
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(false)
   })
+
+  it('should return a throw if compare throws', async () => {
+    const sut = makeSUT()
+    // @ts-ignore
+    jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+    const promise = await sut.compare('any_value', 'any_hash')
+    await expect(promise).rejects.toThrow()
+  })
 })
