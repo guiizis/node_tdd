b1d02274d946bf1892480979de8c6488a66d30fd
guiziis <guigamoda@hotmail.com>
2022-10-17 20:18:50 -0300

    refactor: cria sut para jwtAdapter

--- src/infra/criptography/jwtAdapter/jwtAdapter.spec.ts
+++ src/infra/criptography/jwtAdapter/jwtAdapter.spec.ts
@@ -5,23 +5,27 @@ jest.mock('jsonwebtoken', () => ({
   sign: (): Promise<string> => new Promise(resolve => resolve('any_token'))
 }))
 
+const makeSUT = (): JwtAdapter =>{
+  return new JwtAdapter('secret')
+}
+
 
 describe('jwt adapter tests', () => {
   it('should call sign with correct values', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSUT()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
   })
 
   it('should return a token on a sign success', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSUT()
     const accessToken = await sut.encrypt('any_id')
     expect(accessToken).toBe('any_token')
   })
 
   it('should throw if sign throws', async () => {
-    const sut = new JwtAdapter('secret')
+    const sut = makeSUT()
     
     jest.spyOn(jwt, 'sign').mockImplementationOnce(()=>{
       throw new Error('')
