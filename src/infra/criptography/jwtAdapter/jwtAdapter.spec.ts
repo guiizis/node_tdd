41bb06a58357fe02c0f02dd35da3d49d86cbb2c4
guiziis <guigamoda@hotmail.com>
2022-10-17 20:09:24 -0300

    feat: retorna jwt

--- src/infra/criptography/jwtAdapter/jwtAdapter.spec.ts
+++ src/infra/criptography/jwtAdapter/jwtAdapter.spec.ts
@@ -1,11 +1,22 @@
 import jwt from "jsonwebtoken"
 import { JwtAdapter } from "./jwtAdapter"
 
+jest.mock('jsonwebtoken', () => ({
+  sign: (): Promise<string> => new Promise(resolve => resolve('any_token'))
+}))
+
+
 describe('jwt adapter tests', () => {
-  it('should call sign with correct values', async() => { 
+  it('should call sign with correct values', async () => {
     const sut = new JwtAdapter('secret')
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
-    expect(signSpy).toHaveBeenCalledWith({id: 'any_id'}, 'secret')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  it('should return a token on a sign success', async () => {
+    const sut = new JwtAdapter('secret')
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('any_token')
   })
 })
