87ab0d5e875c71b06da6beaef3eceb70a39d22bb
guiziis <guigamoda@hotmail.com>
2022-10-18 19:43:26 -0300

    test: testa sucesso e falha de loadByEmail

--- src/infra/db/mongodb/accountRepository/account.spec.ts
+++ src/infra/db/mongodb/accountRepository/account.spec.ts
@@ -1,5 +1,8 @@
+import { Collection } from 'mongodb'
 import { MongoHelper } from '../helpers/mongoHelper'
 import { AccountMongoRepository } from './account'
+
+let accountCollection: Collection
 
 describe('account test mongo', () => {
   beforeAll(async () => {
@@ -11,7 +14,7 @@ describe('account test mongo', () => {
   })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -19,7 +22,7 @@ describe('account test mongo', () => {
     return new AccountMongoRepository()
   }
 
-  it('should return an account on success', async () => {
+  it('should return an account on add success', async () => {
     const sut = makeSUT()
     const account = await sut.add({
       name: 'any_name',
@@ -31,5 +34,25 @@ describe('account test mongo', () => {
     expect(account.name).toBe('any_name')
     expect(account.email).toBe('any_email')
     expect(account.password).toBe('any_password')
+  })
+
+  it('should return an account on loadByEmail success', async () => {
+    const sut = makeSUT()
+    await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const account = await sut.loadByEmail('any_email@mail.com')
+    expect(account.name).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.password).toBe('any_password')
+  })
+
+  it('should return null if loadByEmail fail', async () => {
+    const sut = makeSUT()
+    const account = await sut.loadByEmail('any_email@mail.com')
+    expect(account).toBeFalsy()
   })
 })
