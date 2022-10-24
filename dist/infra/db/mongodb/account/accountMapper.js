f41b50447e60d08b1cd05c3d32b0582e3024296c
guiziis <guigamoda@hotmail.com>
2022-10-24 20:22:49 -0300

    fix: remove dist

--- dist/infra/db/mongodb/account/accountMapper.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.map = void 0;
-const map = (account) => {
-    const { _id, ...accountWithouID } = account;
-    return Object.assign({}, accountWithouID);
-};
-exports.map = map;
