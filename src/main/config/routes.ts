bd10085b75439d7f0116d7044078218029f29cc0
guiziis <guigamoda@hotmail.com>
2022-11-07 09:15:39 -0300

    chore: adiciona debug a aplicacao

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,11 +1,12 @@
 import { Express, Router } from 'express'
 import { readdirSync } from 'fs'
+import { join } from 'path'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  readdirSync(`${__dirname}/../routes`).map(async file => {
-    if (!file.includes('.test.')) {
+  readdirSync(join(__dirname, '../routes')).map(async file => {
+    if (!file.endsWith('.test.ts') && !file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
     }
   })
