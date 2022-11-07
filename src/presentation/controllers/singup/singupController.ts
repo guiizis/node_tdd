9e6fd0e0674fa50e95336e8afe90d3bb01524726
guiziis <guigamoda@hotmail.com>
2022-11-07 20:40:22 -0300

    chore: adiciona configs para nodemon

--- src/presentation/controllers/singup/singupController.ts
+++ src/presentation/controllers/singup/singupController.ts
@@ -2,9 +2,9 @@ import { badRequest, ok, serverError } from '../../helper/http/httpHelpers'
 import { Controller, HttpRequest, HttpResponse, AddAccount, Validation } from './singupProtocolsController'
 
 export class SignupController implements Controller {
-  constructor (private readonly addAccount: AddAccount, private readonly validation: Validation) {}
+  constructor(private readonly addAccount: AddAccount, private readonly validation: Validation) { }
 
-  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const error = this.validation.validate(httpRequest.body)
       if (error) {
@@ -17,7 +17,6 @@ export class SignupController implements Controller {
         email,
         password
       })
-
       return ok(account)
     } catch (error) {
       console.error(error)
